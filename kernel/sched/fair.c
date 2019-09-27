7ca3881437894205362e0934742c47f99d16fbd5
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2019-09-27 11:57:19 +0530

    sched: walt: Use sched_avg for WALT only
    sched_avg stats enabled for SMP, but it is used only in WALT.
    So move sched_avg under WALT.
    
    Change-Id: I19b558833e12b80f02fea27c9a9fc8b7630d8689
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    [Lau: adjust for 4.9]
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: Poebe S.A <darkaccent69@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2782,7 +2782,7 @@ static inline void update_cfs_shares(struct sched_entity *se)
 }
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
-#ifdef CONFIG_SMP
+#ifdef CONFIG_SCHED_WALT
 u32 sched_get_wake_up_idle(struct task_struct *p)
 {
 	u32 enabled = p->flags & PF_WAKE_UP_IDLE;
@@ -2803,6 +2803,7 @@ int sched_set_wake_up_idle(struct task_struct *p, int wake_up_idle)
 	return 0;
 }
 EXPORT_SYMBOL(sched_set_wake_up_idle);
+#endif
 
 #ifdef CONFIG_SMP
 /*
