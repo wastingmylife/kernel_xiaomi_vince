ebee1a8e9ebc28a212f020eb12b6c3eb878c1529
Lau <laststandrighthere@gmail.com>
2020-07-20 02:15:21 +0800

    schedutil: Use Pixel's up/down ratelimits values by default
    Having 1000/1000 by default isn't good for mobile devices where
    We want frequencies to increase rapidly but decrease slowly.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: Poebe S.A <darkaccent69@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -702,7 +702,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		ret = -ENOMEM;
 		goto stop_kthread;
 	}
-
+	
+#ifdef CONFIG_SCHED_WALT
 	/*
 	 * NOTE:
 	 * intializing up_rate/down_rate to 0 explicitly in kernel
@@ -710,8 +711,11 @@ static int sugov_init(struct cpufreq_policy *policy)
 	 */
 	tunables->up_rate_limit_us = 0;
 	tunables->down_rate_limit_us = 0;
-
 	tunables->iowait_boost_enable = false;
+#else
+	tunables->up_rate_limit_us = 20000;
+	tunables->down_rate_limit_us = 500;
+#endif
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
