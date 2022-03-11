8fcf24bf128c31564e1c227c93c12e89072ee604
Poebe S.A <darkaccent69@gmail.com>
2022-03-11 12:37:06 +0000

    by.sh: Update buildscript
    Signed-off-by: Poebe S.A <darkaccent69@gmail.com>

--- by.sh
+++ by.sh
@@ -1,26 +1,33 @@
 #! /bin/bash
-# Copyright (C) 2020 StarLight5234
-#
 
+# Copyright (C) 2021-2022 rk134
+# Thanks to eun0115, starlight5234 and ghostmaster69-dev
 export DEVICE="Vince"
 export CONFIG="vince-perf_defconfig"
+export TC_PATH="/workspace/q/toolchains"
 export CHANNEL_ID=""
 export TELEGRAM_TOKEN=""
-export TC_PATH="/workspace/q/toolchains"
-export ZIP_DIR="/workspace/q/anykernel"
-export IS_MIUI="no"
+export ZIP_DIR="$(pwd)/Anykernel3"
 export KERNEL_DIR=$(pwd)
-export KBUILD_BUILD_USER="RocksCI"
-export GCC_COMPILE="yes" 
-export KBUILD_BUILD_HOST="wastingmylife"
-export LINUX_VERSION=$(awk '/SUBLEVEL/ {print $3}' Makefile \
-    | head -1 | sed 's/[^0-9]*//g')
+export GCC_COMPILE="yes"
+export KBUILD_BUILD_USER="wastingmylife"
+export KBUILD_BUILD_HOST="noidea"
 
-#==============================================================
-#===================== Function Definition ====================
-#==============================================================
-#======================= Telegram Start =======================
-#==============================================================
+# FUNCTIONS
+
+# Ask Telegram Channel/Chat ID -- thanks to @Ghostmaster69-dev
+if [[ -z ${CHANNEL_ID} ]]; then
+    echo -n "Sup dude! Please give me your telegram channel ID: "
+    read -r tg_channel_id
+    CHANNEL_ID="${tg_channel_id}"
+fi
+
+# Ask Telegram Bot API Token -- thanks to @Ghostmaster69-dev
+if [[ -z ${TELEGRAM_TOKEN} ]]; then
+    echo -n "Please give me your telegram bot token: "
+    read -r tg_token
+    TELEGRAM_TOKEN="${tg_token}"
+fi
 
 # Upload buildlog to group
 tg_erlog()
@@ -40,6 +47,39 @@ tg_pushzip()
 			-F caption="Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
 }
 
+# Upload download link to channel
+tg_pushlink()
+{
+        export zip_directory="$(cd $(pwd)/AnyKernel3/ && ls *.zip)"
+        curl -s https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$CHANNEL_ID 
+}
+
+# Send Updates
+function tg_sendinfo() {
+	curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+		-d "parse_mode=html" \
+		-d text="${1}" \
+		-d chat_id="${CHANNEL_ID}" \
+		-d "disable_web_page_preview=true"
+}
+
+# Clone the toolchains and export required information
+function clone_tc() {
+[ -d ${TC_PATH} ] || mkdir ${TC_PATH}
+
+if [ "$GCC_COMPILE" == "no" ]; then
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git ${TC_PATH}/clang
+	export PATH="${TC_PATH}/clang/bin:$PATH"
+	export STRIP="${TC_PATH}/clang/aarch64-linux-gnu/bin/strip"
+	export COMPILER="Clang 14.0.0"
+else
+    git clone --depth=1 https://github.com/wastingmylife/gcc64 ${TC_PATH}/gcc64
+	git clone --depth=1 https://github.com/wastingmylife/gcc32 ${TC_PATH}/gcc32
+	export PATH="${TC_PATH}/gcc64/bin:${TC_PATH}/gcc32/bin:$PATH"
+	export STRIP="${TC_PATH}/gcc64/aarch64-elf/bin/strip"
+	export COMPILER="Arter97's GCC Compiler" 
+fi
+}
 # Send Updates
 function tg_sendinfo() {
 	curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
@@ -52,7 +92,7 @@ function tg_sendinfo() {
 # Send a sticker
 function start_sticker() {
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAMPXvdff5azEK_7peNplS4ywWcagh4AAgwBAALQuClVMBjhY-CopowaBA" \
+        -d sticker="CAACAgQAAxkBAAEDIYdhctPrAm1Ydl3sFori9vNNnjAoigAC9AkAAl79YVHW7zfYKT9-XyEE" \
         -d chat_id=$CHANNEL_ID
 }
 
@@ -62,105 +102,39 @@ function error_sticker() {
         -d chat_id=$CHANNEL_ID
 }
 
-#==============================================================
-#======================= Telegram End =========================
-#==============================================================
-#========================= Clone TC ===========================
-#======================== & AnyKernel =========================
-#==============================================================
-
-function clone_tc() {
-[ -d ${TC_PATH} ] || mkdir ${TC_PATH}
-
-if [ "$GCC_COMPILE" == "no" ]; then
-	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git ${TC_PATH}/clang
-	export PATH="${TC_PATH}/clang/bin:$PATH"
-	export STRIP="${TC_PATH}/clang/aarch64-linux-gnu/bin/strip"
-	export COMPILER="Kdrag0n's Proton Clang"
-else
-	git clone --depth=1 https://github.com/wastingmylife/gcc64 ${TC_PATH}/gcc64
-	git clone --depth=1 https://github.com/wastingmylife/gcc32 ${TC_PATH}/gcc32
-	export PATH="${TC_PATH}/gcc64/bin:${TC_PATH}/gcc32/bin:$PATH"
-	export STRIP="${TC_PATH}/gcc64/aarch64-elf/bin/strip"
-	export COMPILER="Arter97's GCC Compiler" 
-fi
-
-rm -rf $ZIP_DIR && git clone https://github.com/wastingmylife/AnyKernel3 $ZIP_DIR
-}
-
-#==============================================================
-#=========================== Make =============================
-#========================== Kernel ============================
-#==============================================================
-
-build_kernel() {
+# Compile this kernel
+function compile() {
 DATE=`date`
 BUILD_START=$(date +"%s")
-make O=out ARCH=arm64 "$CONFIG"
-
-if [ "$GCC_COMPILE" == "no" ]; then
-	make -j$(nproc --all) O=out \
-			      ARCH=arm64 \
-			      AR=llvm-ar \
-			      NM=llvm-nm \
-			      OBJCOPY=llvm-objcopy \
-			      OBJDUMP=llvm-objdump \
-			      STRIP=llvm-strip \
-			      CC=clang \
-			      CROSS_COMPILE=aarch64-linux-gnu- \
-			      CROSS_COMPILE_ARM32=arm-linux-gnueabi- |& tee -a $HOME/build/build${BUILD}.txt
-else
-	make -j$(nproc --all) O=out \
-			      ARCH=arm64 \
-			      CROSS_COMPILE=aarch64-elf- \
-			      CROSS_COMPILE_ARM32=arm-eabi- |& tee -a $HOME/build/build${BUILD}.txt
-fi
+make O=out ARCH=arm64 vince-perf_defconfig
+make -j$(nproc --all) O=out \
+                ARCH=arm64 \
+			    CROSS_COMPILE=aarch64-elf- \
+			    CROSS_COMPILE_ARM32=arm-eabi- |& tee -a $HOME/build/build${BUILD}.txt
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 }
 
-#==============================================================
-#==================== Make Flashable Zip ======================
-#==============================================================
-
+# Zip this kernel
 function make_flashable() {
-
-if [ "$IS_MIUI" == "yes" ]; then
-# credit @adekmaulana
-    for MODULES in $(find "$KERNEL_DIR/out" -name '*.ko'); do
-        "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
-        "$KERNEL_DIR/scripts/sign-file" sha512 \
-                "$KERNEL_DIR/out/signing_key.priv" \
-                "$KERNEL_DIR/out/signing_key.x509" \
-                "${MODULES}"
-        case ${MODULES} in
-                */wlan.ko)
-		cp "${MODULES}" "${VENDOR_MODULEDIR}/pronto_wlan.ko"
-            ;;
-        esac
-    done
-    echo -e "(i) Done moving wifi modules"
-fi
-
+    
 cd $ZIP_DIR
-make clean &>/dev/null
+dir
+rm -rf zIm*
+rm -rf Winds*
 cp $KERN_IMG $ZIP_DIR/zImage
-if [ "$BRANCH" == "stable" ]; then
-	make stable &>/dev/null
-elif [ "$BRANCH" == "wip" ]; then
-	make beta &>/dev/null
+if [ "$BRANCH" == "windstorm" ]; then
+    zip -r9 Windstorm-kernel-[Rivalist].zip * -x .git README.md *placeholder
+elif [ "$BRANCH" == "beta" ]; then
+    zip -r9 Windstorm-kernel-[Test].zip * -x .git README.md *placeholder
 else
-	make test &>/dev/null
+    zip -r9 Windstorm-kernel-[unavailable].zip * -x .git README.md *placeholder
 fi
 ZIP=$(echo *.zip)
 tg_pushzip
 
 }
-
-#==============================================================
-#========================= Build Log ==========================
-#==============================================================
 
 # Credits: @madeofgreat
 BTXT="$HOME/build/buildno.txt" #BTXT is Build number TeXT
@@ -174,59 +148,52 @@ BUILD=$(cat $BTXT)
 BUILD=$(($BUILD + 1))
 echo ${BUILD} > $BTXT
 
-#==============================================================
-#===================== Random sticker =========================
-#==================== for build error =========================
-#==============================================================
-
+# Sticker selection
 stick=$(($RANDOM % 5))
 
 if [ "$stick" == "0" ]; then
-	STICKER="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE"
+	STICKER="CAACAgIAAxkBAAEDIWhhcssHSMR1HTAHtKOby21tVafvWgAC_gADVp29CtoEYTAu-df_IQQ"
 elif [ "$stick" == "1" ];then
-	STICKER="CAACAgQAAxkBAAMRXveCWisHv4FNMrlAacnmFRWSL0wAAgEBAAJyIUgjtWOZJdyKFpMaBA"
+	STICKER="CAACAgIAAxkBAAEDIXlhcsvK31evc58huNXRZnSWf62R2AAC_w4AAhSUAAFL2_NFL9rIYIAhBA"
 elif [ "$stick" == "2" ];then
-	STICKER="CAACAgUAAxkBAAMSXveCj7P1y5I5AAGaH2wt2tMCXuqZAAL_AAO-xUFXBB9-5f3MjMsaBA"
+	STICKER="CAACAgUAAxkBAAEDIXthcsvYV4zwNP0ousx1ULwkKGRdygACIAADYOojP1RURqxGbEhrIQQ"
 elif [ "$stick" == "3" ];then
-	STICKER="CAACAgUAAxkBAAMTXveDSSQq2q8fGrIvpmJ4kPx8T1AAAhEBAALKhyBVEsDSQXY-jrwaBA"
+	STICKER="CAACAgUAAxkBAAEDIX1hcsvr8e6DUr1J4KmHCtI98gx1xwACNgADP9jqMxV1oXRlrlnXIQQ"
 elif [ "$stick" == "4" ];then
-	STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
+	STICKER="CAACAgEAAxkBAAEDIYFhcswQNqw8ZPubg7zGQkNhaYGTBAACKwIAAvx0QESn-U6NZyYYfSEE"
 fi
 
-#==============================================================
-#===================== End of function ========================
-#======================= definition ===========================
-#==============================================================
-
+#-----------------------------------------------------------------------------------------------------------#
 clone_tc
-
 COMMIT=$(git log --pretty=format:'"%h : %s"' -1)
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 KERNEL_DIR=$(pwd)
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 CONFIG_PATH=$KERNEL_DIR/arch/arm64/configs/$CONFIG
 VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-export KERN_VER=$(echo "$(make kernelversion)")
-
-# Cleaning source
+export KERN_VER=$(echo "$(make kernelversion --no-print-directory)")
 make mrproper && rm -rf out
-
 start_sticker
 tg_sendinfo "$(echo -e "======= <b>$DEVICE</b> =======\n
-Build-Host   :- <b>$KBUILD_BUILD_HOST</b>
-Build-User   :- <b>$KBUILD_BUILD_USER</b>\n 
-Version        :- <u><b>$KERN_VER</b></u>
-Compiler      :- <i>$COMPILER</i>\n
-on Branch   :- <b>$BRANCH</b>
-Commit       :- <b>$COMMIT</b>\n")"
+Build-Host   :- <code>$KBUILD_BUILD_HOST</code>
+Build-User   :- <code>$KBUILD_BUILD_USER</code>\n 
+Version      :- <u><code>$KERN_VER</code></u>
+Compiler     :- <code>$COMPILER</code>\n
+on Branch    :- <code>$BRANCH</code>
+Commit       :- <code>$COMMIT</code>\n")"
 
-build_kernel
-
-# Check if kernel img is there or not and make flashable accordingly
-
+compile
 if ! [ -a "$KERN_IMG" ]; then
 	tg_erlog && error_sticker
 	exit 1
 else
 	make_flashable
+	tg_pushlink
 fi
+
+cd $ZIP_DIR
+dir
+rm -rf zIm*
+rm -rf Winds*
+cd ../
+rm -rf out
